7534519ae6c4f1a4c6fa3c841b503ce76fa730c4
liuderchi <liuderchi@gmail.com>
2020-01-30 15:58:04 +0800

    [1] WIP

--- static/lib/main.js
+++ static/lib/main.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 /* globals document, $ */
 
-$(document).ready(function () {
-	/*
+$(document).ready(function() {
+  /*
 		这个文件告诉我们：如何引入一个客户端 js 脚本
 		在 `plugin.json` 中，你可以发现本文件列在 "scripts" 字段中。
 		那个数组的意义是告知 NodeBB 构建时，需要引入并优化的客户端 js 脚本。
@@ -15,8 +15,23 @@ $(document).ready(function () {
 		Ajax 请求完成后触发，更明确的表述： Ajax操作完成， 并切换路由后触发。
 	*/
 
-	// run in home page
-	console.log('in main.js')
-	console.log('nodebb-plugin-quickstart: 已载入');
-	// 注意：这个会在着陆页触发。
+  // run in home page
+  console.log("in main.js");
+  console.log("nodebb-plugin-quickstart: 已载入");
+  // 注意：这个会在着陆页触发。
+
+  // fetch categories
+  let categories = [];
+  if (location && location.href){
+    fetch(`${location.href}api`)
+      .then(r => r.json())
+      .then(data => {
+        categories = (data.categories || []).map(({ name, icon }) => ({
+          name,
+          icon
+        }));
+        console.log({ categories });
+      })
+      .catch(console.error);
+  }
 });
